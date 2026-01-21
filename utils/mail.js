04b3ee061cac794bc628f0a263326428b0d573c1
Abhinav Kumar <kumabhi139@gmail.com>
2026-01-21 13:26:13 +0530

    updated

--- utils/mail.js
+++ utils/mail.js
@@ -1,31 +1,64 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
+
+// const Mail = async (email, subject, htmlBody, textBody) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: process.env.MAIL_HOST,
+//       port: process.env.MAIL_PORT || 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.MAIL_USER, // SMTP LOGIN
+//         pass: process.env.MAIL_PASS, // SMTP KEY
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_EMAIL}>`,
+//       to: email,
+//       subject,
+//       text: textBody,
+//       html: htmlBody,
+//     };
+
+//     const result = await transporter.sendMail(mailOptions);
+//     console.log("✅ Mail sent:", result.messageId);
+//     return result;
+//   } catch (error) {
+//     console.error("❌ Mail sending failed:", error.message);
+//     throw new Error("Email could not be sent. Please try again later.");
+//   }
+// };
+
+// module.exports = Mail;
+
+const SibApiV3Sdk = require("sib-api-v3-sdk");
+
+const client = SibApiV3Sdk.ApiClient.instance;
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const tranEmailApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const Mail = async (email, subject, htmlBody, textBody) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT || 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER, // SMTP LOGIN
-        pass: process.env.MAIL_PASS, // SMTP KEY
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
+      to: [{ email }],
+      sender: {
+        email: process.env.MAIL_FROM_EMAIL,
+        name: process.env.MAIL_FROM_NAME,
       },
+      subject,
+      htmlContent: htmlBody,
+      textContent: textBody,
     });
 
-    const mailOptions = {
-      from: `"${process.env.MAIL_FROM_NAME}" <${process.env.MAIL_FROM_EMAIL}>`,
-      to: email,
-      subject,
-      text: textBody,
-      html: htmlBody,
-    };
+    const result = await tranEmailApi.sendTransacEmail(sendSmtpEmail);
 
-    const result = await transporter.sendMail(mailOptions);
     console.log("✅ Mail sent:", result.messageId);
     return result;
   } catch (error) {
-    console.error("❌ Mail sending failed:", error.message);
+    console.error("❌ Brevo Mail failed:", error?.response?.text || error.message);
     throw new Error("Email could not be sent. Please try again later.");
   }
 };
